a69d99c18b1cc41ed6bd539d39817299df14a151
Roman <korog7789@gmail.com>
2019-11-05 12:20:51 +0400

    v1.4

--- sql.js
+++ sql.js
@@ -2,17 +2,20 @@ const mysql = require("mysql2");
   const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
- database: "sys",
-  password: "protectfly" }).promise();
-const sql = "INSERT INTO users (idusers, userscol) VALUES (?, ?)";
-const user = [ 4,"Stan"];
-connection.query(sql, user)
-          .then(result =>{
-            console.log(result[0]);
-          })
-          .catch(err =>{
-            console.log(err);
-          });
+ database: "userbasa",
+  password: "protectfly" });
+const sql = `create table if not exists users(
+      id int primary key auto_increment,
+      name varchar(255) not null,
+      age int not null
+    )`;
+     connection.query(sql, function(err, results) {
+        if(err) console.log(err);
+        else console.log("Таблица создана");
+    });
+    connection.end();
+    
+
 
 
 
