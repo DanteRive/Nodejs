98cfeac9f9635bc9ee45b5c0c054502e687a28db
Roman <korog7789@gmail.com>
2019-11-05 11:42:37 +0400

    v1.11

--- sql.js
+++ sql.js
@@ -1,7 +1,12 @@
 const mysql = require("mysql2");
-  
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "protectfly"
+  const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "protectfly"
 });
+ connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");    }
+ });
